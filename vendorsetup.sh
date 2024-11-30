406759addf8248a4a13d76b25c1b96a2d75dab3f
improbir <ghosh_probir@yahoo.com>
2024-11-30 04:42:04 +0000

    vendorsetup: Move all branch to A15
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,20 +1,20 @@
 #Kernel Tree
-git clone https://github.com/improbir/kernel_realme_sdm710.git -b 14 kernel/realme/sdm710
+git clone https://github.com/improbir/kernel_realme_sdm710.git -b 15 kernel/realme/sdm710
 
 #Vendor Tree
-git clone https://github.com/improbir/android_vendor_realme_RMX1921.git -b 14 vendor/realme/RMX1921 
+git clone https://github.com/improbir/android_vendor_realme_RMX1921.git -b 15 vendor/realme/RMX1921 
 
 #Clean existing hals
 rm -rf hardware/qcom-caf/sdm845
 
 #Audio hal
-git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-21.0-caf-sdm845 hardware/qcom-caf/sdm845/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-22.0-caf-sdm845 hardware/qcom-caf/sdm845/audio
 
 #Display hal
-git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-21.0-caf-sdm845 hardware/qcom-caf/sdm845/display
+git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-22.0-caf-sdm845 hardware/qcom-caf/sdm845/display
 
 #Media hal
-git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-21.0-caf-sdm845 hardware/qcom-caf/sdm845/media
+git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-22.0-caf-sdm845 hardware/qcom-caf/sdm845/media
 
 #Clang-proton
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git  prebuilts/clang/host/linux-x86/clang-proton
