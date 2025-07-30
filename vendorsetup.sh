96a684f089e0234aefe51695131a40d445cd41ca
YouSummoner <107224555+YouSummoner@users.noreply.github.com>
2025-07-30 21:49:17 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,13 +8,13 @@ git clone https://github.com/improbir/android_vendor_realme_RMX1921.git -b 15 ve
 rm -rf hardware/qcom-caf/sdm845
 
 #Audio hal
-git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-22.1-caf-sdm845 hardware/qcom-caf/sdm845/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-22.2-caf-sdm845 hardware/qcom-caf/sdm845/audio
 
 #Display hal
-git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-22.1-caf-sdm845 hardware/qcom-caf/sdm845/display
+git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-22.2-caf-sdm845 hardware/qcom-caf/sdm845/display
 
 #Media hal
-git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-22.1-caf-sdm845 hardware/qcom-caf/sdm845/media
+git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-22.2-caf-sdm845 hardware/qcom-caf/sdm845/media
 
 #Clang-proton
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git  prebuilts/clang/host/linux-x86/clang-proton
