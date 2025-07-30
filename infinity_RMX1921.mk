7d8c70c033633bb2058850ed754565ace94b7939
YouSummoner <107224555+YouSummoner@users.noreply.github.com>
2025-07-30 21:56:56 +0530

    Update and rename pixelage_RMX1921.mk to infinity_RMX1921.mk

--- infinity_RMX1921.mk
+++ infinity_RMX1921.mk
@@ -2,19 +2,15 @@
 # Copyright (C) 2021-2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
-#
 
-# Inherit some common Pixelage stuff
+# Inherit some common InfinityX stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
-WITH_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
-TARGET_DEFAULT_PIXEL_LAUNCHER := true
-TARGET_HAS_UDFPS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_EXCLUDES_AUDIOFX := true
-PIXELAGE_MAINTAINER := improbir
-$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
+TARGET_SUPPORTS_QUICK_TAP := true
+INFINITY_MAINTAINER := YouSummoner
+WITH_GAPPS := true
+TARGET_SHIPS_GOOGLE_DIALER := true
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -22,7 +18,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := pixelage_RMX1921
+PRODUCT_NAME := infinity_RMX1921
 PRODUCT_MODEL := RMX1921
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
